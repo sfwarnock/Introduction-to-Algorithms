ae1b8c9afeb22a47bbb34223a0e4fcb9987bad4e
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-01-16 06:16:06 -0700

    Add files via upload

---
+++ insertion-sort 2.1-1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 06:06:16 2019
+
+@author: Scott Warnock
+"""
+
+# Pseudocode
+# insertion-sort(A)
+#  for j = 2 to A.length
+#   key = A[j]
+#   // Insert A[j] into the sorted squence A[1..j - 1]
+#   i = j - 1
+#   while i > 0 and A[i] > key
+#       A[i+1] = A[i]
+#       i = i - 1
+#   A[i + 1] = key
