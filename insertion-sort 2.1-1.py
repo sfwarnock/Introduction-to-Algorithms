ad50235ad0b4bf8ec187aa78bbd6298416f11d8d
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-01-30 06:25:02 -0700

    Add files via upload

--- insertion-sort 2.1-1.py
+++ insertion-sort 2.1-1.py
@@ -20,10 +20,12 @@ a = [31, 41, 59, 26, 41, 58]
 
 def insertionSort(a):
     
-    for currentCard in a[1]:
-        cardKey = currentCard
+    for currentCard in a:
+        cardKey = currentCard[1]
         arrayIndex = currentCard - 1
         while arrayIndex > 0 and a[arrayIndex] > cardKey:
-            
-        
-        
+            a[arrayIndex + 1] = a[arrayIndex]
+            arrayIndex =+ 1
+        a[arrayIndex + 1] = cardKey
+
+insertionSort(a)
