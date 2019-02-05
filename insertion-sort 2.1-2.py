4039ce13a058fcf7a6ddafe274e88517e4d32fb3
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-02-05 06:34:34 -0700

    Add files via upload

--- insertion-sort 2.1-2.py
+++ insertion-sort 2.1-2.py
@@ -9,5 +9,17 @@ Created on Tue Feb  5 06:15:53 2019
 
 def decreasingInsertionSort(theArray):
     
-    for arrayIndex in range(-1, len(theArray)):
+    for arrayIndex in range(1, len(theArray)):
         
+        currentArrayValue = theArray[arrayIndex]
+        position = arrayIndex
+        
+        while position > 0 and theArray[position - 1] < currentArrayValue:
+            theArray[position] = theArray[position - 1]
+            position = position - 1
+            
+        theArray[position] = currentArrayValue
+
+theArray = [31, 41, 59, 26, 41, 58, 1, 14, 100, 123, 145]
+decreasingInsertionSort(theArray)
+print(theArray)
