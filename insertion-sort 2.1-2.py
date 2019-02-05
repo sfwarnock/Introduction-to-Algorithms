a042fdc565ef6f8057ceb659e0e47e85d50ea314
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-02-05 06:23:23 -0700

    Add files via upload

--- insertion-sort 2.1-2.py
+++ insertion-sort 2.1-2.py
@@ -5,3 +5,9 @@ Created on Tue Feb  5 06:15:53 2019
 @author: Scott Warnock
 """
 
+# Rewrite insertion-sort 2.1-1 procedure to sort into nonincreasing instead of nondecreasing order.
+
+def decreasingInsertionSort(theArray):
+    
+    for arrayIndex in range(-1, len(theArray)):
+        
