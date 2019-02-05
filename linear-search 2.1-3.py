ec0ca67d008c8bda6a402b44fd471e14cd38a59f
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-02-05 06:39:38 -0700

    Add files via upload

---
+++ linear-search 2.1-3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  5 06:34:49 2019
+
+@author: Scott Warnock
+"""
+
+#   Input: A sequence of n numbers A = [a1, a2,....an] and a value v.
+
+#   Output: An index i such that v = A[i] or the special value NIL if v does not appear in A.
+
+#   Write pseudocode for linear search, which scans through the sequence, looking for v.
+#   Using a loop invariant, prove the your algorithm is correct. Make sure that your
+#   loop invariant fullfills the three necessary properties.
