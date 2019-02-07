6c0f649ef26f23af68f335acbb6e815e13fc2ff7
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-02-07 06:19:28 -0700

    Add files via upload

---
+++ leetcode/leetcode.twosum.easy.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  7 06:15:11 2019
+
+@author: Scott Warnock
+"""
+
+# LeetCode Two Sum
+
+# Given an array of integers, return indices of the two nubmers such that add up to a specific target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
