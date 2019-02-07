78b34911eec585b014ab9590e129847bdc2350ba
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-02-07 06:47:18 -0700

    Add files via upload

--- leetcode/leetcode.twosum.easy.py
+++ leetcode/leetcode.twosum.easy.py
@@ -7,6 +7,19 @@ Created on Thu Feb  7 06:15:11 2019
 
 # LeetCode Two Sum
 
-# Given an array of integers, return indices of the two nubmers such that add up to a specific target.
+# Given an array of integers, return indices of the two nubmers such that they add up to a specific target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
+# Example:
+
+#   given nums = [2, 7, 11, 15], target = 9
+
+#   because nums[0] + nums[1] = 2 + 7 = 9
+#   return [0,1]
+
+def sumTwo():
+    for values in range(0,len(numbersArray)):
+        currentArrayValue == numbersArray[values]
+        targetSum = 9
+        
+        while currentArrayValue > 0 
