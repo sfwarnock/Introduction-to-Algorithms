28b0ee5de1bbee5f25180ee7ae3d293d7fc3eef4
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-02-11 06:34:59 -0700

    Add files via upload

--- leetcode/leetcode.twosum.easy.py
+++ leetcode/leetcode.twosum.easy.py
@@ -17,12 +17,18 @@ Created on Thu Feb  7 06:15:11 2019
 #   because nums[0] + nums[1] = 2 + 7 = 9
 #   return [0,1]
 
-def sumTwo():
+def sumTwo(numbersArray):
     for values in range(0,len(numbersArray)):       
-        currentArrayValue == numbersArray[values]
+        currentArrayValue = numbersArray[values]
+        targetSum = numbersArray + currentArrayValue
         targetValue = 9
                   
         while currentArrayValue > 0 and targetSum != targetValue:
-                
-                
+            if targetSum == targetValue:
+                return currentArrayValue[values] and numbersArray[values]
+            elif targetSum != targetValue:
+                currentArrayValue =+ 1
+            
 numbersArray = [4, 5, 9, 10, 11]
+sumTwo(numbersArray)
+print(sumTwo)
