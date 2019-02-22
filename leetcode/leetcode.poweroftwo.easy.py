3c4eccd7c260239393b788b8d759188fa2d8938c
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-02-22 06:30:28 -0700

    Add files via upload

--- leetcode/leetcode.poweroftwo.easy.py
+++ leetcode/leetcode.poweroftwo.easy.py
@@ -23,3 +23,14 @@ Example 3:
     Output: false
     
 """
+
+def powerOfTwo(n):
+    if n <=0:
+        return False
+
+    while n % 2 == 0:
+        n = n / 2
+        
+    return n== 1
+
+n = 16  
