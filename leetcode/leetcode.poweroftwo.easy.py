664a84910ca532d0a0a23c06ebb1c4b8e89a9e15
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-02-22 06:15:49 -0700

    Add files via upload

---
+++ leetcode/leetcode.poweroftwo.easy.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 22 06:11:28 2019
+
+@author: Scott Warnock
+"""
+
+"""
+Given an integer, write a function to determine if it is a power of two.
+
+Example 1:
+    Input: 1
+    Output: true
+    Explanation: 2^0 = 1
+    
+Example 2:
+    Input: 16
+    Output: true
+    Explanation: 2^4 = 16
+    
+Example 3:
+    Input: 218
+    Output: false
+    
+"""
