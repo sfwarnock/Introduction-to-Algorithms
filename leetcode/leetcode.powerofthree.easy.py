14429c3e6870a1ca844d858fece8925576667aee
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-03-11 06:40:05 -0600

    Add files via upload

--- leetcode/leetcode.powerofthree.easy.py
+++ leetcode/leetcode.powerofthree.easy.py
@@ -13,9 +13,7 @@ def powerOfThree(n):
     
     while n % 3 == 0:
         n = n / 3
-    if n == 1:
-        return True
-    else:
-        return False
+    return True if n==1 else False
+
 n = 729
 powerOfThree(n)
