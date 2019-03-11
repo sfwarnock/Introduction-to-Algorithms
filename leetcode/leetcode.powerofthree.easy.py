6d51776f4fc51e8bd5863b5663394d71c3ec53d0
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-03-11 06:22:26 -0600

    Add files via upload

---
+++ leetcode/leetcode.powerofthree.easy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 06:19:03 2019
+
+@author: Scott Warnock
+"""
+
+# Given an integer, write a function to determine if it is a power of three.
+
+def powerOfThree(n):
+    if n / 3 == 1:
+        print(True)
+
+n = 3
+powerOfThree(n)
