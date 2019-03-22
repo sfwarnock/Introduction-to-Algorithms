9025e5a7634475e0638ba03cb8a6475eb7740de4
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-03-21 18:17:14 -0600

    Add files via upload

--- leetcode/leetcode.twosum_refactored.easy.py
+++ leetcode/leetcode.twosum_refactored.easy.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Mar 18 06:54:10 2019
-
 @author: Scott Warnock
 """
 
@@ -16,20 +15,33 @@ Created on Mon Mar 18 06:54:10 2019
 
 #   because nums[0] + nums[1] = 2 + 7 = 9
 #   return [0,1]
+'''
+add array to dict key, value equals index.
+
+k = key[v = 0] - target
+
+while key[v] >= 0:
+    if k in d:
+        return key[v = 0], k,v
+    else:
+        key[v + 1]
+
+
+    d = {a[i]: i for i in range(len(a))}
+'''
 
 def sumTwo(a, target):
-
     d = {a[i]: i for i in range(len(a))}
     i = 0
     
-    while d:
+    while True:
         j = target - a[i]
-        if j in d:    
+        if j in d and j != a[i]:    
             print (i, d[j])
             break
         else:
             i += 1
-    
-target = 26
-a = [2, 7, 11, 15]
+        
+target = 6
+a = [3, 2, 4]
 sumTwo(a, target)
