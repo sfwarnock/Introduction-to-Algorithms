49305863e74e2d77f0d9ad786973b5e4b588d19c
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-03-28 06:38:32 -0600

    Add files via upload

---
+++ leetcode/leetcode.reverseVowel.easy.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 06:34:45 2019
+
+@author: Scott Warnock
+"""
+"""
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+
+Input: "hello"
+Output: "holle"
+
+Example 2:
+
+Input: "leetcode"
+Output: "leotcede"
+
+Note:
+The vowels does not include the letter "y".
+""""
+
+def reverseVowel(vowels):
+    
+vowels = [a, e, i, o, u]
