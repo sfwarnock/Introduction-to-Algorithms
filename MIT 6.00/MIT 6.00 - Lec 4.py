465e58605b4b3ab6402e36aee00ca41413d432d0
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-04-09 06:56:05 -0600

    Add files via upload

--- MIT 6.00/MIT 6.00 - Lec 4.py
+++ MIT 6.00/MIT 6.00 - Lec 4.py
@@ -4,9 +4,9 @@ Created on Thu Apr  4 06:24:01 2019
 
 @author: Scott Warnock
 """
-
+"""
 def sqrt(x):
-    """Specification: Returns the square root of x, if x is a perfect square."""
+    """"""Specification: Returns the square root of x, if x is a perfect square.""""""
     ans = 0
     
     if x >= 0:
@@ -66,3 +66,30 @@ def barnYard1():
         print('Number of spiders: ', spiders)
 
 barnYard1()
+"""
+
+def solve2(numLegs, numHeads):
+    solutionFound = False
+    for numSpiders in range(0, numHeads + 1):
+        for numChicks in range(0, numHeads - numSpiders + 1):
+            numPigs = numHeads - numChicks
+            totLegs = 4 * numPigs + 2 * numChicks + 8 * numSpiders
+            if totLegs == numLegs:
+                print('Number of Pigs: ', str(numPigs) + ',',)
+                print('Number of Chickens: ', str(numChicks) + ',',)
+                print('Number of Spiders: ', numSpiders)
+                solutionFound = True
+    if not solutionFound: print('There is no solution.')
+    
+def barnYard2():
+    heads = int(input('Enter the number of heads: '))
+    legs = int(input('Enter the number of legs: '))
+    pigs, chickens, spiders = solve2(legs, heads)
+    if pigs == None:
+        print('There is no solution')
+    else:
+        print('Number of pigs: ', pigs)
+        print('Number of chickens: ', chickens)
+        print('Number of spiders: ', spiders)
+
+barnYard2()
