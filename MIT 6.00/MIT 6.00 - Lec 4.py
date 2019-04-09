4e7b5d8c35214324d3407aa02ac22be9ac1e24fa
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-04-09 06:31:12 -0600

    Add files via upload

--- MIT 6.00/MIT 6.00 - Lec 4.py
+++ MIT 6.00/MIT 6.00 - Lec 4.py
@@ -6,6 +6,7 @@ Created on Thu Apr  4 06:24:01 2019
 """
 
 def sqrt(x):
+    """Specification: Returns the square root of x, if x is a perfect square."""
     ans = 0
     
     if x >= 0:
@@ -20,5 +21,23 @@ def sqrt(x):
         print ('x is a negative number')
         return None
     
-sqrt(16)
+sqrt(25)
 
+
+def solve(numLegs, numHeads):
+    for numChicks in range(0, numHeads + 1):
+        numPigs = numHeads - numChicks
+        totLegs = (4 * numbPigs) + (2 * numChicks)
+        if totLegs == numLegs:
+            return [numPigs, numChicks]
+        return [None, None]
+
+def barnYard():
+    heads = int(input('Enter the number of heads: '))
+    legs = int(input('Enter the number of legs: '))
+    pigs, chickens = solve(legs, heads)
+    if pigs == None:
+        print('There is no solution')
+    else:
+        print('Number of pigs: ', pigs)
+        print('Number of chickens: ', chickens)
