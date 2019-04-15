6716d17b8cb41e2ce38ce1592c9d8cd0138040b5
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-04-15 06:59:13 -0600

    Add files via upload

--- MIT 6.00/MIT 6.00 - Lec 5.py
+++ MIT 6.00/MIT 6.00 - Lec 5.py
@@ -5,3 +5,40 @@ Created on Mon Apr 15 06:13:36 2019
 @author: Scott Warnock
 """
 
+#b = 2**999
+#print(b)
+
+#x = 0.1
+#print(x)
+
+"""
+s = 0.0
+for i in range(10):
+    s += 0.1
+    print(s)
+"""
+    
+import math
+a = math.sqrt(2)
+print(a)
+
+
+def squareRootBi(x, epsilon):
+    """Assumes x >= 0 and epsilon = 0
+        Return y s.t. y*y is within epsilion of x"""
+    assert x >= 0, 'x must be non-negative, not' + str(x)
+    assert epsilon > 0, 'epsilon must be positive, not' + str(epsilon)
+    low = 0
+    high = x
+    ctr = 1
+    while abs(guess**2 - x) > epsilon and ctr <= 100:
+        print('low:', low, 'high:', high, 'guess:', guess)
+        if guess**2 < x:
+            low = guess
+        else:
+            high = guess
+        guess = (low + high)/2.0
+        ctr += 1
+    assert ctr <= 100, 'Iteration count exceeded'
+    print ('Bi method. Num. interations:', ctr, 'Estimate:', guess)
+    return guess
