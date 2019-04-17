b5d5cfa4c277d0bf9f741d2e65b93f0c3744a9d1
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-04-17 06:51:45 -0600

    Add files via upload

--- MIT 6.00/MIT 6.00 - Lec 6.py
+++ MIT 6.00/MIT 6.00 - Lec 6.py
@@ -37,3 +37,22 @@ def testBi():
     squareRootBi(.25, 0.001)
     
 testBi()
+
+def squareRootNR(x, epsilon):
+    """Assumes x >= 0 and epsilon > 0
+        Return y s.t. y*y is within epsilion of x"""
+    assert x >= 0, 'x must be non-negative, not' + str(x)
+    assert epsilon > 0, 'epsilon must be positive, not' + str(epsilon)
+    x = float(x)
+    guess = x/2.0
+    guess = 0.001
+    diff = guess**2 - x
+    ctr = 1
+    while abs(diff) > epsilon and ctr <= 100:
+        print('Error', diff,'guess:', guess)
+        guess = guess - diff/(2.0*guess)
+        diff = guess**2 - x
+        ctr += 1
+    assert ctr <= 100, 'Iteration count exceeded'
+    print ('Bi method. Num. interations:', ctr, 'Estimate:', guess)
+    return guess
