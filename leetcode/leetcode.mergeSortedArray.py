419bede649d39f922073803043de21d46408778f
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-05-16 06:31:20 -0600

    Add files via upload

--- leetcode/leetcode.mergeSortedArray.py
+++ leetcode/leetcode.mergeSortedArray.py
@@ -26,15 +26,21 @@ Output: [1,2,2,3,5,6]
 def mergeSort(a1, a2):  
     #sort each array
     for index in range(len(a1)):
-        
-        subArray = index
-        position = subArray
     
-        print(position)
+        subArray = a1[index]
+        position = index
+        
+        while position > 0 and a1[position - 1] > subArray:
+            a1[position] = a1[position - 1]
+            position = position - 1
+    
+        a1[position] = subArray
+
     
     #merge a2 into a1
         
 a1 = [9, 1, 3, 7, 5]
 a2 = [4, 2, 10, 6, 8]
 
-mergeSort(a1, a2)   
+mergeSort(a1, a2)
+print(a1)
