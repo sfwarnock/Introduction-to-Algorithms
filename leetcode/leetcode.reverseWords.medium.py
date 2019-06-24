cc6d950fd689f7e264196486af5dd2475a18f0ef
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-06-24 13:56:42 -0600

    Add files via upload

--- leetcode/leetcode.reverseWords.medium.py
+++ leetcode/leetcode.reverseWords.medium.py
@@ -19,14 +19,14 @@ Explanation: Your reversed string should not contain leading or trailing spaces.
 
 Example 3:
 
-Input: "a good   example"
+Input: "a good example"
 Output: "example good a"
 Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
 
 """
 
 def reverseWords(s):
-    
-    
+    for words in s:
+         
 
 s = "reverse this string"
