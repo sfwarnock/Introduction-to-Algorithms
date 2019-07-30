62c99b937b7d0ab99471f38484fde56869f6c96d
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-07-30 15:02:41 -0600

    Add files via upload

--- comprehensions.py
+++ comprehensions.py
@@ -5,3 +5,13 @@ Created on Tue Jul 30 13:52:08 2019
 @author: Scott Warnock
 """
 
+squares = [0, 1, 4, 9, 25]
+print(squares)
+
+squaresLoop = []
+for num in range(6):
+    squaresLoop.append(num*num)
+print(squaresLoop)
+
+squaresSingleLine = [num*num for num in range(6)]
+print(squaresSingleLine)
