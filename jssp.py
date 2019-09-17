eb376f4a4b0d65d8c808b1b03c0a97720965cbda
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-09-17 06:54:35 -0600

    Add files via upload

---
+++ jssp.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  9 06:39:48 2019
+
+@author: sawarno
+"""
+
+"""
+JOB DATA
+j = job
+o = operation
+p = process time
+m = machine 
+r = release date
+f(t) = cost function
+d = due date
+w = weight
+
+i, n = index, number
+
+
+JOB CHARACTERISITICS
+a (alpha)= machine enviroment
+    o if a job must on dedicated system
+    P if P there are identical parallel machines
+    Q if Q there are uniform parallel machines 
+    R if R unrelated parallel machines
+    PMPM or QMPM multipurpose machines with indentical and uniform speeds
+    G multi-operational model, general shop
+    X multi-operational model, mix of job shop and open shop
+    O multi-operational model, open shop
+    J multi-operational model, job shop
+    F multi-operational model, flow shop
+
+
+b (beta) = job characterisitcs
+b1 = preemption (job spliting allowed)
+    b1 = pmtn if allowed
+    b1 != present if not allowed
+
+    b2 = precidence relationships
+
+g (gamma) = optimality criterion
