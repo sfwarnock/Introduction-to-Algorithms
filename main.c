7f594db61eb39ac9f1a5576908db5af32cf4f3bf
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2020-11-30 10:57:31 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  primeN
+//
+//  Created by Warnock, Scott A on 11/30/20.
+//  Copyright © 2020 Warnock, Scott A. All rights reserved.
+//
+
+#include <stdio.h>
+
+#define N 1000
+
+main()
+{
+    int i, j, a[N+1];
+    for (a[1] = 0, i = 2; i <= N; i++) a[i] = 1;
+    for (i = 2; i <= N/2; j++)
+        for (j = 2; j <= N/i; j++)
+            a[i*j] = 0;
+    for (i = 1; i <= N; i++)
+        if (a[i]) printf("%4d", i);
+    printf("\n");
+}
