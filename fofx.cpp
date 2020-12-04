fcddfef96ae46babc6f00f8fdd30ec95411731ec
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2020-12-04 11:31:51 -0700

    Add files via upload

--- fofx.cpp
+++ fofx.cpp
@@ -16,12 +16,26 @@ int f(int x){ // subroutine
         return 2 * f(x-1) + x * x; // recurssive f(x)
 }
 
+/*
+int m(int y){ //subroutine
+    y = 2;
+    
+    return y + f(x);
+}
+*/
+ 
 int main() { // main driver
     int x;
+    int y;
     
     cout << "Enter an x value for f(x)" << endl;
     cin >> x;
     
     cout << "f(" << x << ") = " << f(x) << endl;
+    
+    y = f(x);
+    
+    cout << "The x, y coordinates of f(" << x << ") are: (" << x << "," << y << ")" << endl;
+    
     return 0;
 }
